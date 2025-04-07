da659f9d4ea0f1000e1d22ef1202962b2a596eb4
Nick Ludlam <nick@recoil.org>
2025-04-07 16:54:00 +0100

    Add gamma correction and basepath

--- Scripts/compress_tiles.sh
+++ Scripts/compress_tiles.sh
@@ -4,9 +4,14 @@
 # using ImageMagick. This script will create a new directory called
 # Compressed_LODS with the compressed images.
 
+BASE_DIR=$1
+
 # Set source and destination directories
-SOURCE_DIR="LODS"
-DEST_DIR="Compressed_LODS"
+SOURCE_DIR_NAME="LODS"
+DEST_DIR_NAME="Compressed_LODS"
+
+SOURCE_DIR="$BASE_DIR/$SOURCE_DIR_NAME"
+DEST_DIR="$BASE_DIR/$DEST_DIR_NAME"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
@@ -27,7 +32,8 @@ find "$SOURCE_DIR" -type f -name "*.jpg" | while read -r file; do
     # -quality 85: Compress with 85% quality (adjust as needed)
     # -colorspace sRGB: Ensure consistent color space
     mogrify -strip \
-            -quality 85 \
+            -gamma 0.7 \
+            -quality 75 \
             -sampling-factor 4:2:0 \
             -colorspace sRGB \
             -interlace Plane \
